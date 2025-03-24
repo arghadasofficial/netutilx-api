31779e843b72615a0595a5c51ffff81031ce244e
Argha Das <developer.arghadas@gmail.com>
2025-03-24 14:53:46 +0530

    core_updated

--- src/Helpers/DnsHelper.php
+++ src/Helpers/DnsHelper.php
@@ -52,32 +52,32 @@ class DnsHelper
 
     function aQuery($domain, $server)
     {
-        return executeQuery("dig @$server A $domain +noall +answer");
+        return $this->executeQuery("dig @$server A $domain +noall +answer");
     }
 
     function nsQuery($domain, $server)
     {
-        return executeQuery("dig @$server NS $domain +noall +answer");
+        return $this->executeQuery("dig @$server NS $domain +noall +answer");
     }
 
     function mxQuery($domain, $server)
     {
-        return executeQuery("dig @$server MX $domain +noall +answer");
+        return $this->executeQuery("dig @$server MX $domain +noall +answer");
     }
 
     function soaQuery($domain, $server)
     {
-        return executeQuery("dig @$server SOA $domain +noall +answer");
+        return $this->executeQuery("dig @$server SOA $domain +noall +answer");
     }
 
     function txtQuery($domain, $server)
     {
-        return executeQuery("dig @$server TXT $domain +noall +answer");
+        return $this->executeQuery("dig @$server TXT $domain +noall +answer");
     }
 
     function ptrQuery($ip)
     {
-        return executeQuery("dig -x $ip +noall +answer");
+        return $this->executeQuery("dig -x $ip +noall +answer");
     }
 
 
