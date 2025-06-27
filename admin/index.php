0839331d6db79f740d59bd59b58b47939d052cd8
Argha Das <developer.arghadas@gmail.com>
2025-06-27 23:19:26 +0530

    Initialize index.php file

---
+++ admin/index.php
@@ -0,0 +1,23 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Config\Database;
+
+$database = new Database();
+$db = $database->connect();
+
+$query = "SELECT * FROM " . $this->table_name . " ORDER BY name ASC";
+
+$result = $this->conn->query($query);
+
+$typesArray = $result->fetch_all(MYSQLI_ASSOC);
+
+$result->free();
+
+print_r($typesArray);
+
+?>
+
+<?php require 'partials/head.inc.php'; ?>
+
+<?php require 'partials/foot.inc.php'; ?>
