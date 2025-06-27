1a2f348fc76c1d9a627a303636394415ab8c2e63
Argha Das <developer.arghadas@gmail.com>
2025-06-27 23:22:37 +0530

    Move error handling and header inclusion to the top of index.php for improved readability and execution flow

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,11 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require 'partials/head.inc.php'; 
+?>
+
+<?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Config\Database;
@@ -17,7 +24,5 @@ $result->free();
 print_r($typesArray);
 
 ?>
-
-<?php require 'partials/head.inc.php'; ?>
 
 <?php require 'partials/foot.inc.php'; ?>
